5a9eba5a0bf37925d1f5be66688b58c56001de35
YUAN-DL <747285133@qq.com>
2022-12-09 00:52:30 +0300

    Add Lab5

---
+++ Project5/ServerASP/TestASP/Migrations/20221127110957_InitialCreate.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestASP.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HashCodes",
+                columns: table => new
+                {
+                    ImageId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    hashkey = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HashCodes", x => x.ImageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    ImageId = table.Column<int>(name: "_ImageId", type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    byteimage = table.Column<byte[]>(name: "byte_image", type: "BLOB", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.ImageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    ResultId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    res = table.Column<double>(type: "REAL", nullable: false),
+                    EmotionName = table.Column<string>(type: "TEXT", nullable: false),
+                    ImageId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.ResultId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HashCodes");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "Results");
+        }
+    }
+}
---
+++ Project5/ServerASP/TestASP/Models/Image.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestASP.Models
+{
+    public class _Image
+    {
+        public int _ImageId { get; set; }
+        public byte[] byte_image { get; set; }
+    }
+    public class Result
+    {
+        public int ResultId { get; set; }
+
+        public double res { get; set; }
+
+        public string EmotionName { get; set; }
+
+        public int ImageId { get; set; }
+    }
+    public class Hashcode
+    {
+        public int hashkey { get; set; }
+
+        [Key]
+        [ForeignKey(nameof(_Image))]
+        public int ImageId { get; set; }
+    }
+}
